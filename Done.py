2fa7c7bb576becb5920481bd2c34978ba2033596
jbpatel <jbpatel@iastate.edu>
2017-05-15 20:19:55 -0500

    Files for LT

--- Done.py
+++ Done.py
@@ -4,7 +4,7 @@ from pprint import pprint
 with open('PhotoID.json') as data_file:    
     data = json.load(data_file)
 
-pprint(albumId)
-pprint(id)
-pprint(title)
-pprint(title)
+pprint(albumId) #print the number of the ID
+pprint(id) #print the id of the text file
+pprint(title) #print the title of the text file
+pprint(title) #print the next title of the same number
